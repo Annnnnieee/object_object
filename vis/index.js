e4763be54ae67a44d1b9c378cb972910928c337d
annnnnieee <anastasiya.vt.s@gmail.com>
2019-11-24 11:57:47 -0800

    added draw graph method

--- vis/index.js
+++ vis/index.js
@@ -20,7 +20,7 @@ the graph variable below is just for testing for now
 
 
 
-var graph = {
+var repo = {
     timeline: [
         {   
             commit: "d9aw2a0",
@@ -83,49 +83,12 @@ var graph = {
     ]
 }
 
-nodes = graph.timeline[0].nodes;
-links = graph.timeline[0].links;
+// ==========================================
+// 
+//==========================================
 
-
-
-var link = svg.append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(links)
-    .enter().append("line");
-
-var node = svg.append("g")
-    .attr("class", "nodes")
-    .selectAll("circle")
-    .data(nodes)
-    .enter().append("circle")
-    .attr("r", 2.5)
-    .call(d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended));
-
-node.append("title")
-    .text(function (d) { return d.id; });
-
-simulation
-    .nodes(nodes)
-    .on("tick", ticked);
-
-simulation.force("link")
-    .links(links);
-
-function ticked() {
-    link
-        .attr("x1", function (d) { return d.source.x; })
-        .attr("y1", function (d) { return d.source.y; })
-        .attr("x2", function (d) { return d.target.x; })
-        .attr("y2", function (d) { return d.target.y; });
-
-    node
-        .attr("cx", function (d) { return d.x; })
-        .attr("cy", function (d) { return d.y; });
-}
+drawGraph(repo.timeline[0]);
+// ===========================================
 
 
 function dragstarted(d) {
@@ -143,4 +106,47 @@ function dragended(d) {
     if (!d3.event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
+}
+
+function drawGraph(graph){
+
+var link = svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter().append("line");
+
+var node = svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("circle")
+    .attr("r", 2.5)
+    .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+node.append("title")
+    .text(function (d) { return d.id; });
+
+simulation
+    .nodes(graph.nodes)
+    .on("tick", ticked);
+
+simulation.force("link")
+    .links(graph.links);
+
+    function ticked() {
+        link    
+            .attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
+    
+        node
+            .attr("cx", function (d) { return d.x; })
+            .attr("cy", function (d) { return d.y; });
+    }
+
 }
