bc6879889b1316a2987b6d98c57e13b065612737
ctman96 <ctman96@students.cs.ubc.ca>
2019-11-25 20:27:12 -0800

    tweaks

--- oo.sh
+++ oo.sh
@@ -1,8 +1,8 @@
 # run with sh oo.sh <https git link>
-git clone "$1"
+git clone "$1" lol
 
 cd "lol"
-git log | grep -m 6 commit | cut -c8- | sed 's/^/(cd lol ; git checkout /' | sed 's/$/;git fetch) \'$'\n(cd .. ; node .\/oo\/410\-analysis\/index.js \".\/oo\/lol\/\")/' > ./../commits.sh
+git log | grep -m 6 commit | cut -c8- | sed 's/^/(cd lol ; git checkout /' | sed 's/$/;git fetch) \'$'\n(node .\/410\-analysis\/index.js \".\/lol\/\")/' > ./../commits.sh
 cd ..
 sh commits.sh
 
